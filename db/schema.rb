75d1e122d9e006845bc070032fd1cfa9a7661288
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-01-12 15:27:31 +0200

    exercise 5 / week 1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140112122732) do
+ActiveRecord::Schema.define(version: 20140112125833) do
+
+  create_table "beers", force: true do |t|
+    t.string   "name"
+    t.string   "style"
+    t.integer  "brewery_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "breweries", force: true do |t|
     t.string   "name"
