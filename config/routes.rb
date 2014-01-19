a3f70e046ac2ea68909f0ffc18247e8d89fd08e0
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-01-19 14:36:49 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Ratebeer::Application.routes.draw do
   resources :breweries
 
   root 'breweries#index'
-  get 'kaikki_bisset', to: 'beers#index'
-  get 'ratings', to: 'ratings#index'
-  get 'ratings/new', to:'ratings#new'
-  post 'ratings', to: 'ratings#create'
+  #get 'kaikki_bisset', to: 'beers#index'
+  #get 'ratings', to: 'ratings#index'
+  #get 'ratings/new', to:'ratings#new'
+  #post 'ratings', to: 'ratings#create'
+
+  resources :ratings, :only => [:index, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
