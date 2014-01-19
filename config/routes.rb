fb87ca7a81421e83c08b5bb637f634f8620ff850
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-01-19 14:40:31 +0200

    delete rating

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Ratebeer::Application.routes.draw do
   #get 'ratings/new', to:'ratings#new'
   #post 'ratings', to: 'ratings#create'
 
-  resources :ratings, :only => [:index, :new, :create]
+  resources :ratings, :only => [:index, :new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
