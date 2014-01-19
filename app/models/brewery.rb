184ba7a0bbac2fc0365c43e1a12ded9172635aef
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-01-19 14:59:40 +0200

    exercise 14 / week 2

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,9 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, :dependent => :destroy
   has_many :ratings, :through => :beers
+
+  def average_rating
+    ratings.average(:score)
+  end
+
 end
