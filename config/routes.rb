68bc6d1cb72d75ad11fc810d4fe90b601e8adbfd
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-01-27 12:51:37 +0200

    exercise 2 / week 3

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Ratebeer::Application.routes.draw do
 
   get 'signup', to: 'users#new'
 
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
+
   #get 'kaikki_bisset', to: 'beers#index'
   #get 'ratings', to: 'ratings#index'
   #get 'ratings/new', to:'ratings#new'
