3d3bef2dd490d1ba5f3bb0cd4bb0fdbc984ddf80
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-01-27 18:28:41 +0200

    exercise 12 / week 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy if current_user == @user
+    session[:user_id] = nil
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
