12ecdf450f802ffaebe2cc3310cc65103af8aa2c
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-02-03 19:49:12 +0200

    exercise 6 / week 4

--- spec/features/ratings_spec.rb
+++ spec/features/ratings_spec.rb
@@ -24,4 +24,30 @@ describe "Rating" do
     expect(beer1.ratings.count).to eq(1)
     expect(beer1.average_rating).to eq(15.0)
   end
+
+  describe "on the beer page" do
+
+    it "shows no ratings when there are none" do
+      visit beer_path(beer2)
+      expect(page).to have_content 'beer has not yet been rated!'
+    end
+
+    it "shows rating when there is one" do
+      beer1.ratings << FactoryGirl.create(:rating)
+
+      visit beer_path(beer1)
+      expect(page).to have_content "Has 1 rating"
+      expect(page).to have_content "average 10.0"
+    end
+
+    it "shows rating when there are some" do
+      beer1.ratings << FactoryGirl.create(:rating)
+      beer1.ratings << FactoryGirl.create(:rating2)
+
+      visit beer_path(beer1)
+      expect(page).to have_content "Has 2 ratings"
+      expect(page).to have_content "average 15.0"
+    end
+
+  end
 end
