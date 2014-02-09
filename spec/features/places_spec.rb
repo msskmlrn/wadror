add0d3ed47fbe6fe3e0cf709471dff12d6026712
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-02-09 12:38:58 +0200

    exercise 2 / week 5

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -12,4 +12,29 @@ describe "Places" do
 
     expect(page).to have_content "Oljenkorsi"
   end
+
+  it "if more than one is returned by the API, all of them are shown on the page" do
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        [ Place.new(:name => "Oljenkorsi"), Place.new(:name => "Testibaari") ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Testibaari"
+  end
+
+  it "if no results are returned by the API, 'No locations in <>' is shown on the page" do
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        [ ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "No locations in kumpula"
+  end
 end
